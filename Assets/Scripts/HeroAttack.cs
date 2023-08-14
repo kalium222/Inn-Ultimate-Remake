96988dd2ebaba06209641b9dd07528b9adadc292
kalium222 <kalium222@sjtu.edu.cn>
2023-08-14 15:43:31 +0800

    feat: attack object

--- Assets/Scripts/HeroAttack.cs
+++ Assets/Scripts/HeroAttack.cs
@@ -7,9 +7,13 @@ public class HeroAttack : MonoBehaviour
     private HeroController heroController;
     private HeroInteraction heroInteraction;
     private Animator heroAnimator;
+    private const string Melee_TAG = "MeleeAttack";
+    private const string TRIGGER = "Attack";
+    private const string WEAPON_KIND = "WeaponKind";
     // Handler in inspector
     public GameObject MeleeAttack;
     public bool isAttacking = false;
+    public float attackRadius = 0f;
 
     private void Awake() {
         heroController = GetComponent<HeroController>();
@@ -32,6 +36,12 @@ public class HeroAttack : MonoBehaviour
     
     private void Start() {
         Weapon.OnWeaponAttack += OnWeaponAttack;
+        if (MeleeAttack == null) {
+            throw new System.Exception("MeleeAttack is null");
+        }
+        if (MeleeAttack.GetComponent<MeleeAttack>() == null) {
+            throw new System.Exception("MeleeAttack doesn't have MeleeAttack.cs");
+        }
     }
 
     private void OnDestroy() {
@@ -39,28 +49,31 @@ public class HeroAttack : MonoBehaviour
     }
 
     private void OnWeaponAttack(Weapon.weaponsKind kind, bool isSilver) {
-        Debug.Log("OnWeaponAttack");
         StartCoroutine(MeleeAttackCoroutine(kind, isSilver));
     }
 
     private IEnumerator MeleeAttackCoroutine(Weapon.weaponsKind kind, bool isSilver) {
-        // wait a frame?
-        // yield return null;
-
         // disable movement
         heroController.CanMove = false;
         heroInteraction.CanInteract = false;
         
         // set animation
         isAttacking = true;
-        heroAnimator.SetTrigger("Attack");
-        heroAnimator.SetInteger("WeaponKind", (int)kind);
+        heroAnimator.SetTrigger(TRIGGER);
+        heroAnimator.SetInteger(WEAPON_KIND, (int)kind);
         // wait for a frame
         yield return null;
-        while (heroAnimator.GetCurrentAnimatorStateInfo(0).IsTag("MeleeAttack")) {
+        // instantiate attack gameobject
+        Vector3 targetposition = transform.position;
+        targetposition.x += attackRadius*heroController.lookDirection;
+        GameObject attack = Instantiate(MeleeAttack, targetposition, Quaternion.identity);
+        attack.GetComponent<MeleeAttack>().isSilver = isSilver;
+        attack.GetComponent<MeleeAttack>().kind = (MeleeAttack.MeleeAttackKind)kind;
+        while (heroAnimator.GetCurrentAnimatorStateInfo(0).IsTag(Melee_TAG)) {
             yield return null;
         }
         isAttacking = false;
+        Destroy(attack);
         
 
         // enable movement
