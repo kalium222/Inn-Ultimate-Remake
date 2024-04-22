d6188e9814fac065da0ded6af80f9ad13ca857c1
kalium <kalium222@outlook.com>
2024-04-22 11:28:04 +0800

    feat: abstract base for singleton monobehaviours

---
+++ Assets/Scripts/Utils/SingletonMono.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public abstract class StaticInstanceMono<T> : MonoBehaviour
+                                        where T : MonoBehaviour {
+    public static T Instance { get; private set; }
+    protected virtual void Awake() => Instance = this as T;
+    protected virtual void OnApplicationQuit() {
+        Instance = null;
+        Destroy(gameObject);
+    }
+}
+
+public abstract class TempSingletonMono<T> : StaticInstanceMono<T>
+                                            where T : MonoBehaviour {
+    protected override void Awake() {
+        if ( Instance!=null ) Destroy(gameObject);
+        base.Awake();
+    }
+}
+
+public abstract class SingletonMono<T> : TempSingletonMono<T>
+                                        where T : MonoBehaviour {
+    protected override void Awake() {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+}
+
